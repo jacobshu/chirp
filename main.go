3f04986cd764d2aed88a5d30dcccb287c9a9b5c3
Jacob Shulenberger <jacob@builtdigital.io>
2025-02-06 17:03:58 -0600

    feat: database ironing, codegen, user handlers

--- main.go
+++ main.go
@@ -1,32 +1,66 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"slices"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jacobshu/chirp/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
+	platform       string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("error loading postgres: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
 	serveMux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 
-	fmt.Print("starting server...")
-	apiCfg := apiConfig{}
+	fmt.Print("starting server...\n")
+	apiCfg := apiConfig{
+		db:       dbQueries,
+		platform: platform,
+	}
 
 	serveMux.HandleFunc("GET /api/healthz", healthHandler)
 	serveMux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+	serveMux.HandleFunc("POST /api/users", apiCfg.userHandler)
+
 	serveMux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
 	serveMux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+
 	fileServerHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
 	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
 
@@ -50,12 +84,49 @@ func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
 	cfg.fileserverHits = atomic.Int32{}
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "Forbidden")
+		return
+	} else {
+		cfg.db.Reset(req.Context())
+	}
 }
 
 func healthHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) userHandler(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		fmt.Printf("error during param decode: %v\n", err)
+		respondWithError(w, http.StatusInternalServerError, "something went wrong")
+		return
+	}
+
+	user, err := cfg.db.CreateUser(req.Context(), params.Email)
+	usrData := User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	}
+
+	if err != nil {
+		fmt.Printf("error during query: %v\n", err)
+		respondWithError(w, http.StatusInternalServerError, "something went wrong")
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, usrData)
 }
 
 func validateChirpHandler(w http.ResponseWriter, req *http.Request) {
@@ -71,6 +142,7 @@ func validateChirpHandler(w http.ResponseWriter, req *http.Request) {
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
+		fmt.Printf("error during param decode: %v\n", err)
 		respondWithError(w, http.StatusInternalServerError, "something went wrong")
 		return
 	}
@@ -111,6 +183,7 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 
 	errMsg, err := json.Marshal(errorBody)
 	if err != nil {
+		fmt.Printf("error during respondWithError: %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("something went wrong"))
 		return
@@ -125,6 +198,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 
 	data, err := json.Marshal(payload)
 	if err != nil {
+		fmt.Printf("error during respondWithJSON: %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("something went wrong"))
 		return
